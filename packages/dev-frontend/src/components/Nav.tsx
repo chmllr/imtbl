42dcb4a7cb0e8507a7461823770b3a19129a36f7
Christian Müller <christian@dfinity.org>
2022-08-13 13:29:46 +0200

    modifications

--- packages/dev-frontend/src/components/Nav.tsx
+++ packages/dev-frontend/src/components/Nav.tsx
@@ -6,7 +6,6 @@ export const Nav: React.FC = () => {
     <Box as="nav" sx={{ display: ["none", "flex"], alignItems: "center", flex: 1 }}>
       <Flex>
         <Link to="/">Dashboard</Link>
-        <Link to="/farm">Farm</Link>
       </Flex>
       <Flex sx={{ justifyContent: "flex-end", mr: 3, flex: 1 }}>
         <Link sx={{ fontSize: 1 }} to="/risky-troves">
